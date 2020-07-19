c2bca1794b6975d5957361867eae08b50921cd08
Lokomolo <marcinhenzel96@gmail.com>
2020-07-19 22:42:31 +0200

    basic setup and lazyload directive for images

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { LazyLoadDirective } from './directives/lazy-load.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    MainPageComponent,
+    LazyLoadDirective
   ],
   imports: [
     BrowserModule,
---
+++ portfolio/src/app/directives/lazy-load.directive.ts
@@ -0,0 +1,32 @@
+import { Directive, HostBinding, Input, ElementRef, AfterViewInit } from '@angular/core';
+
+@Directive({
+  selector: '[appLazyLoad]'
+})
+export class LazyLoadDirective implements AfterViewInit {
+  @HostBinding('attr.src') srcAttr = null;
+  @Input() src: string;
+  constructor(private elRef: ElementRef) { }
+
+  ngAfterViewInit() {
+    this.canLazyLoad() ? this.lazyLoadImage() : this.loadImage()
+  }
+
+  private canLazyLoad() {
+    return window && 'IntersectionObserver' in window;
+  }
+  private loadImage() {
+    this.srcAttr = this.src;
+  }
+  private lazyLoadImage() {
+    const obs = new IntersectionObserver(entries => {
+      entries.forEach(({ isIntersecting }) => {
+        if (isIntersecting) {
+          this.loadImage();
+          obs.unobserve(this.elRef.nativeElement);
+        }
+      });
+    })
+    obs.observe(this.elRef.nativeElement);
+  }
+}
